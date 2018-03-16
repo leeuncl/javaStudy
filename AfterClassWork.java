5fdf2f706a277f8032c5459d63db3601a9f844cd
leeuncl <33394342+leeuncl@users.noreply.github.com>
2018-03-16 16:59:00 +0800

    Add files via upload

---
+++ AfterClassWork.java
@@ -0,0 +1,27 @@
+//从控制台输入学员李明3门课的成绩，编写程序实现：
+//	1、 语文、数学、英语的分数之和
+//  2、 3门课的平均分
+//  3、 语文比数学高多少分
+
+import java.util.Scanner;
+
+public class AfterClassWork {
+	public static void main(String[] args) {
+		Scanner st = new Scanner(System.in);
+		System.out.println("请输入语文成绩：");
+		int chineseS = st.nextInt();
+		System.out.println("请输入数学成绩：");
+		int mathS = st.nextInt();
+		System.out.println("请输入英语成绩：");
+		int englishS = st.nextInt();
+		int scoreSum = chineseS + mathS + englishS;		//成绩之和
+		float averge = scoreSum / 3;		//成绩平均分
+		System.out.println("3门课的成绩之和是 " + (chineseS + mathS + englishS) + "分。");
+		System.out.println("3门课的成绩之和是 " + scoreSum + "分。");
+		System.out.println("3门课的平均分是 " + averge);
+		if (chineseS >= mathS)
+			System.out.println("语文成绩比数学成绩高 " + (chineseS - mathS) + "分。");
+		else
+			System.out.println("语文成绩没有数学成绩高！");
+	}
+}
---
+++ AfterClassWork2.java
@@ -0,0 +1,22 @@
+//AfterClassWork
+//从控制台输入学员李明3门课的成绩，编写程序实现：
+//	1、 语文、数学、英语的分数之和
+//  2、 3门课的平均分
+//  3、 语文比数学高多少分
+
+public class AfterClassWork2{
+	public static void main(String args[]){
+		int chineseS, mathS, englishS, scoreSum;
+		chineseS = 90;
+		mathS = 99;
+		englishS = 100;
+		scoreSum = chineseS + mathS + englishS;
+		System.out.println("The sum score is:" + scoreSum);
+		float average = scoreSum / 3;
+		System.out.println("The average is:" + average);
+		if (chineseS >= mathS)
+			System.out.println("chineseScore is more" + (chineseS - mathS) + "than mathScore");
+		else
+			System.out.println("chineseScore isn't high than mathScore");
+	}
+}
